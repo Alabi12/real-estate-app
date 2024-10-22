f677f028a82f0eb423ff9503d6864e2d8c7481be
Robert O. Alabi <wolo22001@gmail.com>
2024-10-22 22:16:49 +0000

    add roles to users

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,14 +1,30 @@
 class PropertiesController < ApplicationController
+  before_action :set_property, only: %i[show edit update destroy]
+
   def index
     @properties = Property.all
-    if params[:search].present?
-      @properties = @properties.where("location LIKE ?", "%#{params[:search]}%")
-    end
-    @properties = @properties.page(params[:page]).per(9)  # Add pagination
   end
+
   def show
-    @property = Property.find(params[:id])
   end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to properties_path, notice: 'Property was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # This action is now defined, and it uses the set_property callback to find the record
+  end
+
   def search
     @properties = Property.all
     @properties = @properties.where("location LIKE ?", "%#{params[:search]}%") if params[:search].present?
@@ -16,4 +32,31 @@ class PropertiesController < ApplicationController
     @properties = @properties.where("price <= ?", params[:max_price]) if params[:max_price].present?
     @properties = @properties.page(params[:page]).per(9)
   end
+
+  def update
+    if @property.update(property_params)
+      redirect_to properties_path, notice: 'Property was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_path, notice: 'Property was successfully deleted.'
+  end
+
+  private
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:name, :location, :price, :description, images: [])
+  end
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'home#index'
   
   resources :agents, only: [:index, :new, :create, :show]
-  resources :properties, only: [:index, :new, :create, :show]
+  resources :properties
   get 'search', to: 'properties#search', as: :search_properties
 
   get 'contact', to: 'static_pages#contact', as: :contact
